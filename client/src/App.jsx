050048aa7114f790ca48d0295daa398feec63331
Logicless-Coder <abhirathore565@gmail.com>
2022-05-07 01:48:58 +0530

    Added Login template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<div className="App w-screen min-h-screen">
 			<Navbar />
+			<Login />
 		</div>
 	);
 }
