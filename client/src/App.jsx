8741add873d7defc07725d1ca1a9303f5490e824
Logicless-Coder <abhirathore565@gmail.com>
2022-05-07 00:51:43 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import logo from "./logo.svg";
+import { useState } from "react";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return <div className="App"></div>;
+}
+
+export default App;
