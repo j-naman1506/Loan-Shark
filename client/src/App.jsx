268928ceb35f1c3286200308be8f2827269ee986
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2022-05-07 14:57:12 +0530

    i

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,23 +4,13 @@ import Login from "../src/Components/LoginComps/login";
 import Register from "../src/Components/LoginComps/Register";
 import Navbar from "./Components/NavbarComps/Navbar";
 import Home from "./Components/DashboardComps/Home";
-import HandleLogin from "./Components/LoginComps/HandleLoginPanels";
+
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Router>
         <Routes>
-          {/* <Route
-            path="/login"
-            exact
-            render={(props) => <HandleLogin loginPanel {...props} />}
-          />
-          <Route
-            path="/register"
-            exact
-            render={(props) => <HandleLogin {...props} />}
-          /> */}
           <Route path="/" exact element={<Home />} />
           <Route path="/login" exact element={<Login />} />
           <Route path="/register" exact element={<Register />} />
