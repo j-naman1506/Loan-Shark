bf9ed2b55d3a3c4379e6f0f62acf6f676df24167
Logicless-Coder <abhirathore565@gmail.com>
2022-05-08 23:20:35 +0530

    Modified Navbar a little

--- client/src/Components/NavbarComps/Navbar.jsx
+++ client/src/Components/NavbarComps/Navbar.jsx
@@ -1,6 +1,31 @@
-// Chakra UI components which uses tailwaind to create a navbar
+import { useSelector } from "react-redux";
+
+const routes = [
+	{
+		path: "/login",
+		name: "Login",
+		protected: false,
+	},
+	{
+		path: "/register",
+		name: "Register",
+		protected: false,
+	},
+	{
+		path: "/app",
+		name: "Application",
+		protected: true,
+	},
+	{
+		path: "/",
+		name: "Profile",
+		protected: true,
+	},
+];
 
 const Navbar = () => {
+	const authToken = useSelector((state) => state.auth.token);
+
 	return (
 		<div className="bg-shade-700 py-4 px-32 flex justify-between items-center shadow-lg">
 			<div className="font-bold text-white text-3xl font-ubuntu">
@@ -8,16 +33,13 @@ const Navbar = () => {
 			</div>
 			<div className="flex-grow">
 				<ul className="flex justify-end gap-10 text-shade-100">
-					<li>
-						<a href="/login" className="opacity-60 hover:opacity-100">
-							Login
-						</a>
-					</li>
-					<li>
-						<a href="/register" className="opacity-60 hover:opacity-100">
-							Register
-						</a>
-					</li>
+					{routes.map((route, index) => (
+						<li key={index}>
+							<a href={route.path} className="opacity-60 hover:opacity-100">
+								{route.name}
+							</a>
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
