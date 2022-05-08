18bc536995054595f5857a3b360907347c01cd06
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2022-05-08 17:00:48 +0530

    aadhar state manage

--- client/src/Components/DashboardComps/EditAadharDetails.jsx
+++ client/src/Components/DashboardComps/EditAadharDetails.jsx
@@ -1,163 +1,227 @@
 import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 import {
-	Button,
-	ButtonGroup,
-	FormControl,
-	FormLabel,
-	Heading,
-	Input,
+  Button,
+  ButtonGroup,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
 } from "@chakra-ui/react";
 
 import { shade } from "./../../static/templates/colors";
-
+import { useState, useEffect } from "react";
 const EditAadharDetails = ({
-	stage,
-	setStage,
-	stopEditing,
-	profile,
-	setProfile,
+  stage,
+  setStage,
+  stopEditing,
+  profile,
+  setProfile,
 }) => {
-	return (
-		<>
-			<div className="bg-shade-200 h-full w-full rounded-r-xl py-6 px-12 flex flex-col gap-4">
-				<Heading as="h2" size="xl" textAlign="center">
-					Aadhar & PAN Details
-				</Heading>
-				<form className="flex flex-col h-full justify-evenly gap-8 ">
-					<FormControl>
-						<FormLabel htmlFor="aadhar_number">Aadhar Number</FormLabel>
-						<Input
-							id="aadhar_number"
-							type="text"
-							name="aadhar_number"
-							borderColor={shade[800]}
-							_hover={{ borderColor: shade[900] }}
-							_active={{ borderColor: shade[900] }}
-							borderWidth={1}
-						/>
-					</FormControl>
+  const [peronsalDetails, setDetails] = useState();
+  useEffect(() => {
+    setDetails(profile);
+  }, []);
 
-					<FormControl>
-						<FormLabel htmlFor="aadhar_card">
-							Upload your Aadhar Card in PDF format
-						</FormLabel>
-						<Input
-							id="aadhar_card"
-							type="file"
-							accept="application/pdf"
-							name="aadhar_card"
-							fontSize="lg"
-							padding="1"
-							borderColor={shade[800]}
-							_hover={{ borderColor: shade[900] }}
-							_active={{ borderColor: shade[900] }}
-							borderWidth={1}
-						/>
-					</FormControl>
+  function handleChange(e) {
+    setDetails((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+  function handleFileChange(e) {
+    setDetails((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.files[0],
+    }));
+  }
+  function handleContinue() {
+    console.log(peronsalDetails);
+    setProfile((prevState) => ({
+      ...profile,
+      ...peronsalDetails,
+    }));
+    setStage(stage + 1);
+    console.log(profile);
+  }
+  function handleBack() {
+    console.log(peronsalDetails);
+    setProfile((prevState) => ({
+      ...profile,
+      ...peronsalDetails,
+    }));
+    setStage(stage - 1);
+    console.log(profile);
+  }
+  return (
+    peronsalDetails && (
+      <>
+        <div className="bg-shade-200 h-full w-full rounded-r-xl py-6 px-12 flex flex-col gap-4">
+          <Heading as="h2" size="xl" textAlign="center">
+            Aadhar & PAN Details
+          </Heading>
+          <form className="flex flex-col h-full justify-evenly gap-8 ">
+            <FormControl>
+              <FormLabel htmlFor="aadhar_number">Aadhar Number</FormLabel>
+              <Input
+                id="aadhar_number"
+                type="text"
+                name="aadhar_number"
+                borderColor={shade[800]}
+                _hover={{ borderColor: shade[900] }}
+                _active={{ borderColor: shade[900] }}
+                borderWidth={1}
+                value={
+                  peronsalDetails.aadhar_number
+                    ? peronsalDetails.aadhar_number
+                    : ""
+                }
+                onChange={handleChange}
+              />
+            </FormControl>
 
-					<FormControl>
-						<FormLabel htmlFor="pan">PAN</FormLabel>
-						<Input
-							id="pan"
-							type="text"
-							name="pan"
-							borderColor={shade[800]}
-							_hover={{ borderColor: shade[900] }}
-							_active={{ borderColor: shade[900] }}
-							borderWidth={1}
-						/>
-					</FormControl>
+            <FormControl>
+              <FormLabel htmlFor="aadhar_card">
+                Upload your Aadhar Card in PDF format
+              </FormLabel>
+              <Input
+                id="aadhar_card"
+                type="file"
+                accept="application/pdf"
+                name="aadhar_card"
+                fontSize="lg"
+                padding="1"
+                borderColor={shade[800]}
+                _hover={{ borderColor: shade[900] }}
+                _active={{ borderColor: shade[900] }}
+                borderWidth={1}
+                src={
+                  peronsalDetails.aadhar_card
+                    ? peronsalDetails.aadhar_card
+                    : null
+                }
+                onChange={handleFileChange}
+              />
+            </FormControl>
 
-					<FormControl>
-						<FormLabel htmlFor="pan_card">
-							Upload your PAN Card in PDF format
-						</FormLabel>
-						<Input
-							id="pan_card"
-							type="file"
-							accept="application/pdf"
-							name="pan_card"
-							fontSize="lg"
-							padding="1"
-							borderColor={shade[800]}
-							_hover={{ borderColor: shade[900] }}
-							_active={{ borderColor: shade[900] }}
-							borderWidth={1}
-						/>
-					</FormControl>
+            <FormControl>
+              <FormLabel htmlFor="pan">PAN</FormLabel>
+              <Input
+                id="pan_number"
+                type="text"
+                name="pan_number"
+                borderColor={shade[800]}
+                _hover={{ borderColor: shade[900] }}
+                _active={{ borderColor: shade[900] }}
+                borderWidth={1}
+                value={
+                  peronsalDetails.pan_number ? peronsalDetails.pan_number : ""
+                }
+                onChange={handleChange}
+              />
+            </FormControl>
 
-					<ButtonGroup spacing="6">
-						<Button
-							leftIcon={<ArrowBackIcon />}
-							bgColor={shade[200]}
-							textColor={shade[800]}
-							borderWidth={1}
-							borderColor={shade[800]}
-							_hover={{
-								bgColor: shade[500],
-								textColor: shade[900],
-								borderColor: shade[900],
-								borderWidth: 1,
-							}}
-							size="md"
-							onClick={() => setStage(stage - 1)}
-						>
-							Back
-						</Button>
-						<Button
-							rightIcon={<ArrowForwardIcon />}
-							bgColor={shade[800]}
-							textColor={shade[200]}
-							borderWidth={1}
-							borderColor={shade[100]}
-							_hover={{
-								bgColor: shade[500],
-								textColor: shade[900],
-								borderColor: shade[900],
-								borderWidth: 1,
-							}}
-							size="md"
-							onClick={() => setStage(stage + 1)}
-						>
-							Continue
-						</Button>
-						<Button
-							bgColor={shade[200]}
-							textColor={shade[800]}
-							borderWidth={1}
-							borderColor={shade[800]}
-							_hover={{
-								bgColor: shade[500],
-								textColor: shade[900],
-								borderColor: shade[900],
-								borderWidth: 1,
-							}}
-							size="md"
-						>
-							Clear
-						</Button>
+            <FormControl>
+              <FormLabel htmlFor="pan_card">
+                Upload your PAN Card in PDF format
+              </FormLabel>
+              <Input
+                id="pan_card"
+                type="file"
+                accept="application/pdf"
+                name="pan_card"
+                fontSize="lg"
+                padding="1"
+                borderColor={shade[800]}
+                _hover={{ borderColor: shade[900] }}
+                _active={{ borderColor: shade[900] }}
+                borderWidth={1}
+                src={peronsalDetails.pan_card ? peronsalDetails.pan_card : null}
+                onChange={handleFileChange}
+              />
+            </FormControl>
 
-						<Button
-							bgColor={shade[200]}
-							textColor={shade[800]}
-							borderWidth={1}
-							borderColor={shade[800]}
-							_hover={{
-								bgColor: shade[500],
-								textColor: shade[900],
-								borderColor: shade[900],
-								borderWidth: 1,
-							}}
-							size="md"
-							onClick={stopEditing}
-						>
-							Cancel
-						</Button>
-					</ButtonGroup>
-				</form>
-			</div>
-		</>
-	);
+            <ButtonGroup spacing="6">
+              <Button
+                leftIcon={<ArrowBackIcon />}
+                bgColor={shade[200]}
+                textColor={shade[800]}
+                borderWidth={1}
+                borderColor={shade[800]}
+                _hover={{
+                  bgColor: shade[500],
+                  textColor: shade[900],
+                  borderColor: shade[900],
+                  borderWidth: 1,
+                }}
+                size="md"
+                onClick={handleBack}
+              >
+                Back
+              </Button>
+              <Button
+                rightIcon={<ArrowForwardIcon />}
+                bgColor={shade[800]}
+                textColor={shade[200]}
+                borderWidth={1}
+                borderColor={shade[100]}
+                _hover={{
+                  bgColor: shade[500],
+                  textColor: shade[900],
+                  borderColor: shade[900],
+                  borderWidth: 1,
+                }}
+                size="md"
+                onClick={handleContinue}
+              >
+                Continue
+              </Button>
+              <Button
+                bgColor={shade[200]}
+                textColor={shade[800]}
+                borderWidth={1}
+                borderColor={shade[800]}
+                _hover={{
+                  bgColor: shade[500],
+                  textColor: shade[900],
+                  borderColor: shade[900],
+                  borderWidth: 1,
+                }}
+                size="md"
+                onClick={() => {
+                  setDetails({
+                    ...profile,
+                    aadhar_number: "",
+                    aadhar_card: "",
+                    pan_number: "",
+                    pan_card: "",
+                  });
+                }}
+              >
+                Clear
+              </Button>
+
+              <Button
+                bgColor={shade[200]}
+                textColor={shade[800]}
+                borderWidth={1}
+                borderColor={shade[800]}
+                _hover={{
+                  bgColor: shade[500],
+                  textColor: shade[900],
+                  borderColor: shade[900],
+                  borderWidth: 1,
+                }}
+                size="md"
+                onClick={stopEditing}
+              >
+                Cancel
+              </Button>
+            </ButtonGroup>
+          </form>
+        </div>
+      </>
+    )
+  );
 };
 
 export default EditAadharDetails;
--- client/src/Components/DashboardComps/EditProfile.jsx
+++ client/src/Components/DashboardComps/EditProfile.jsx
@@ -1,60 +1,60 @@
 import EditAadharDetails from "./EditAadharDetails";
 import EditBankDetails from "./EditBankDetails";
-import EditPaySlips from "./EditPaySlips";
+// import EditPaySlips from "./EditPaySlips";
 import EditProfileDetails from "./EditProfileDetails";
 import { useState } from "react";
 
 const EditProfile = ({ stage, setStage, profile, setProfile, stopEditing }) => {
-	const renderStage = (_stage) => {
-		switch (_stage) {
-			case 0:
-				return (
-					<EditProfileDetails
-						stage={stage}
-						setStage={setStage}
-						profile={profile}
-						setProfile={setProfile}
-						stopEditing={stopEditing}
-					/>
-				);
-			case 1:
-				return (
-					<EditBankDetails
-						stage={stage}
-						setStage={setStage}
-						profile={profile}
-						setProfile={setProfile}
-						stopEditing={stopEditing}
-					/>
-				);
-			case 2:
-				return (
-					<EditAadharDetails
-						stage={stage}
-						setStage={setStage}
-						profile={profile}
-						setProfile={setProfile}
-						stopEditing={stopEditing}
-					/>
-				);
-			case 3:
-				return (
-					<EditPaySlips
-						stage={stage}
-						setStage={setStage}
-						profile={profile}
-						setProfile={setProfile}
-						stopEditing={stopEditing}
-					/>
-				);
-		}
-	};
+  const renderStage = (_stage) => {
+    switch (_stage) {
+      case 0:
+        return (
+          <EditProfileDetails
+            stage={stage}
+            setStage={setStage}
+            profile={profile}
+            setProfile={setProfile}
+            stopEditing={stopEditing}
+          />
+        );
+      case 1:
+        return (
+          <EditBankDetails
+            stage={stage}
+            setStage={setStage}
+            profile={profile}
+            setProfile={setProfile}
+            stopEditing={stopEditing}
+          />
+        );
+      case 2:
+        return (
+          <EditAadharDetails
+            stage={stage}
+            setStage={setStage}
+            profile={profile}
+            setProfile={setProfile}
+            stopEditing={stopEditing}
+          />
+        );
+      case 3:
+        return (
+          <EditPaySlips
+            stage={stage}
+            setStage={setStage}
+            profile={profile}
+            setProfile={setProfile}
+            stopEditing={stopEditing}
+          />
+        );
+    }
+  };
 
-	return (
-		<>
-			<div className="bg-shade-1 w-full h-full flex">{renderStage(stage)}</div>
-		</>
-	);
+  return (
+    <>
+      <div className="bg-shade-1 w-full h-full flex">{renderStage(stage)}</div>
+    </>
+  );
 };
 
 export default EditProfile;
