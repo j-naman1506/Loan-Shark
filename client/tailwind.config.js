a5dfedb0e318c5c5138c9a4f3a2d3cf07c3522b1
Logicless-Coder <abhirathore565@gmail.com>
2022-05-08 11:10:44 +0530

    Added a new color shade as 'shade' under tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,11 +3,22 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				"shade-0": "#FFF9EC",
-				"shade-1": "#FFDCCC",
-				"shade-2": "#FCB1A6",
-				"shade-3": "#FB6376",
-				"shade-4": "#5D2A42",
+				// "shade-0": "#FFF9EC",
+				// "shade-1": "#FFDCCC",
+				// "shade-2": "#FCB1A6",
+				// "shade-3": "#FB6376",
+				// "shade-4": "#5D2A42",
+				shade: {
+					100: "#D8F3DC",
+					200: "#B7E4C7",
+					300: "#95D5B2",
+					400: "#74C69D",
+					500: "#52B788",
+					600: "#40916C",
+					700: "#2D6A4F",
+					800: "#1B4332",
+					900: "#081C15",
+				},
 			},
 			fontFamily: {
 				ubuntu: ["Ubuntu", "sans-serif"],
