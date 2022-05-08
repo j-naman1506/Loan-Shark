0f14fba17a938a5f78580753e647e955f067b583
jainsahab-1506 <2019UCP1390@mnit.ac.in>
2022-05-08 09:45:47 +0530

    login and register integration

--- client/src/Components/utils/requests.js
+++ client/src/Components/utils/requests.js
@@ -1,4 +1,8 @@
 export const SITE_URL = process.env.REACT_APP_SITE_URL;
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-export const requests = {};
+export const requests = {
+  doRegister: "/auth/add-user/",
+  doLogin: "/auth/login/",
+  doOAuthLogin: "/auth/social/google-oauth2/",
+};
