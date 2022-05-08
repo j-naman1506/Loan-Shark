f9f09bc04c11285b2dc2293122ec563b7ff7285a
Logicless-Coder <abhirathore565@gmail.com>
2022-05-08 17:35:22 +0530

    Finished with implementing PDF upliad previews

--- client/src/Components/DashboardComps/EditAadharDetails.jsx
+++ client/src/Components/DashboardComps/EditAadharDetails.jsx
@@ -1,14 +1,25 @@
-import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+import {
+	ArrowBackIcon,
+	ArrowForwardIcon,
+	ArrowLeftIcon,
+	ArrowRightIcon,
+} from "@chakra-ui/icons";
 import {
 	Button,
 	ButtonGroup,
 	FormControl,
 	FormLabel,
 	Heading,
+	IconButton,
 	Input,
+	Text,
 } from "@chakra-ui/react";
 
+import { Viewer } from "@react-pdf-viewer/core";
 import { shade } from "./../../static/templates/colors";
+import { useState } from "react";
 
 const EditAadharDetails = ({
 	stage,
@@ -17,6 +28,19 @@ const EditAadharDetails = ({
 	profile,
 	setProfile,
 }) => {
+	const [aadharUrl, setAadharUrl] = useState(null);
+	const [panUrl, setPanUrl] = useState(null);
+
+	const onAadharChange = (e) => {
+		const files = e.target.files;
+		files.length > 0 && setAadharUrl(URL.createObjectURL(files[0]));
+	};
+
+	const onPanChange = (e) => {
+		const files = e.target.files;
+		files.length > 0 && setPanUrl(URL.createObjectURL(files[0]));
+	};
+
 	return (
 		<>
 			<div className="bg-shade-200 h-full w-full rounded-r-xl py-6 px-12 flex flex-col gap-4">
@@ -24,67 +48,93 @@ const EditAadharDetails = ({
 					Aadhar & PAN Details
 				</Heading>
 				<form className="flex flex-col h-full justify-evenly gap-8 ">
-					<FormControl>
-						<FormLabel htmlFor="aadhar_number">Aadhar Number</FormLabel>
-						<Input
-							id="aadhar_number"
-							type="text"
-							name="aadhar_number"
-							borderColor={shade[800]}
-							_hover={{ borderColor: shade[900] }}
-							_active={{ borderColor: shade[900] }}
-							borderWidth={1}
-						/>
-					</FormControl>
+					<div className="flex gap-4">
+						<FormControl>
+							<FormLabel htmlFor="aadhar_number">Aadhar Number</FormLabel>
+							<Input
+								id="aadhar_number"
+								type="text"
+								name="aadhar_number"
+								borderColor={shade[800]}
+								_hover={{ borderColor: shade[900] }}
+								_active={{ borderColor: shade[900] }}
+								borderWidth={1}
+							/>
+						</FormControl>
+						<FormControl>
+							<FormLabel htmlFor="pan">PAN</FormLabel>
+							<Input
+								id="pan"
+								type="text"
+								name="pan"
+								borderColor={shade[800]}
+								_hover={{ borderColor: shade[900] }}
+								_active={{ borderColor: shade[900] }}
+								borderWidth={1}
+							/>
+						</FormControl>
+					</div>
 
-					<FormControl>
-						<FormLabel htmlFor="aadhar_card">
-							Upload your Aadhar Card in PDF format
-						</FormLabel>
-						<Input
-							id="aadhar_card"
-							type="file"
-							accept="application/pdf"
-							name="aadhar_card"
-							fontSize="lg"
-							padding="1"
-							borderColor={shade[800]}
-							_hover={{ borderColor: shade[900] }}
-							_active={{ borderColor: shade[900] }}
-							borderWidth={1}
-						/>
-					</FormControl>
+					<div className="flex gap-4">
+						<FormControl>
+							<FormLabel htmlFor="aadhar_card">
+								Upload your Aadhar Card in PDF format
+							</FormLabel>
+							<Input
+								id="aadhar_card"
+								type="file"
+								accept="application/pdf"
+								onChange={onAadharChange}
+								name="aadhar_card"
+								fontSize="lg"
+								padding="1"
+								borderColor={shade[800]}
+								_hover={{ borderColor: shade[900] }}
+								_active={{ borderColor: shade[900] }}
+								borderWidth={1}
+							/>
+						</FormControl>
 
-					<FormControl>
-						<FormLabel htmlFor="pan">PAN</FormLabel>
-						<Input
-							id="pan"
-							type="text"
-							name="pan"
-							borderColor={shade[800]}
-							_hover={{ borderColor: shade[900] }}
-							_active={{ borderColor: shade[900] }}
-							borderWidth={1}
-						/>
-					</FormControl>
+						<FormControl>
+							<FormLabel htmlFor="pan_card">
+								Upload your PAN Card in PDF format
+							</FormLabel>
+							<Input
+								id="pan_card"
+								type="file"
+								accept="application/pdf"
+								onChange={onPanChange}
+								name="pan_card"
+								fontSize="lg"
+								padding="1"
+								borderColor={shade[800]}
+								_hover={{ borderColor: shade[900] }}
+								_active={{ borderColor: shade[900] }}
+								borderWidth={1}
+							/>
+						</FormControl>
+					</div>
 
-					<FormControl>
-						<FormLabel htmlFor="pan_card">
-							Upload your PAN Card in PDF format
-						</FormLabel>
-						<Input
-							id="pan_card"
-							type="file"
-							accept="application/pdf"
-							name="pan_card"
-							fontSize="lg"
-							padding="1"
-							borderColor={shade[800]}
-							_hover={{ borderColor: shade[900] }}
-							_active={{ borderColor: shade[900] }}
-							borderWidth={1}
-						/>
-					</FormControl>
+					<div className="flex h-full gap-4">
+						{aadharUrl ? (
+							<div className="h-96 m-0 p-0 w-full overflow-y-scroll">
+								<Viewer fileUrl={aadharUrl} />
+							</div>
+						) : (
+							<div className="flex justify-center items-center h-full w-full border-2 border-shade-900 border-dashed border-opacity-30 text-3xl">
+								Your uploaded Aadhar Card will show here
+							</div>
+						)}
+						{panUrl ? (
+							<div className="h-96 m-0 p-0 w-full overflow-y-scroll">
+								<Viewer fileUrl={panUrl} />
+							</div>
+						) : (
+							<div className="flex justify-center items-center h-full w-full border-2 border-shade-900 border-dashed border-opacity-30 text-3xl">
+								Your uploaded PAN Card will show here
+							</div>
+						)}
+					</div>
 
 					<ButtonGroup spacing="6">
 						<Button
