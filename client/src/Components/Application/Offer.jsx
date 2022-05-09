8b2fdd67fda5999261db129818d0e4e9a68012d2
Logicless-Coder <abhirathore565@gmail.com>
2022-05-09 23:42:37 +0530

    Completed <Dues /> UI. Ready for integration.

--- client/src/Components/Application/Offer.jsx
+++ client/src/Components/Application/Offer.jsx
@@ -20,31 +20,7 @@ const Offer = ({ offer }) => {
 					<Text>
 						<b>Rate:</b> {offer[0].interest * 100}% per month
 					</Text>
-
-					{/* <Icon
-            as={expanded ? MdClose : MdExpandMore}
-            w={8}
-            h={8}
-            onClick={() => setExpanded(!expanded)}
-          /> */}
 				</div>
-				{/* <div
-					className={`${
-						expanded ? "h-full flex flex-col" : "h-0 "
-					} transition-all ease-in-out overflow-y-hidden`}
-				>
-					{offer.slice(1).map((offer) => (
-						<div className="border-t border-t-shade-900 p-4 flex gap-4">
-							<Text className="text-xl font-ubuntu">₹ {offer.principle}</Text>
-							<Text className="text-lg font-roboto">
-								<b>Tenure:</b> {offer.tenure} months
-							</Text>
-							<Text className="text-lg font-roboto">
-								<b>Rate:</b> {offer.interest * 100}% per month
-							</Text>
-						</div>
-					))}
-				</div> */}
 			</div>
 		</>
 	);
