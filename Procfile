8ec1c7319ca414388aa9674da41a05615f156666
Whik4 <amanm3620@gmail.com>
2022-05-10 20:47:07 +0530

    updated procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: python backend/loan_app_server/manage.py runserver 0.0.0.0:8000
+web: python backend/loan_app_server/manage.py runserver 0.0.0.0:$PORT
+release: python backend/loan_app_server/manage.py migrate
 clock: python backend/loan_app_server/clock.py
--- backend/loan_app_server/loan_app_server/settings.py
+++ backend/loan_app_server/loan_app_server/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3!k5jn8naz_by6aq2&k2l@rjsx_9^h7_6-z*w&=_q!vl_^zko_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
